9f032949308e2bbd7d687891190b35545fc46991
Chris Honiball <chris.p.honiball@gmail.com>
2014-04-13 15:28:29 -0500

    Upgraded to express 4

--- app.js
+++ app.js
@@ -48,41 +48,13 @@ db.once('open', function() {
 		content: String
 	});
 
-
-
 	var Post = mongoose.model('Post', postSchema);
-
-	var postOne = new Post({
-		title: "Here's my first post",
-		tags: ['sample', 'another', 'one more'],
-		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, totam nulla minus consequuntur doloremque cupiditate perspiciatis obcaecati itaque. Consequatur officia accusamus necessitatibus tenetur facere hic temporibus ab tempora minima veniam."
-	});
-
-	var postTwo = new Post({
-		title: "A second post",
-		tags: ['Hello', 'world', 'how are you'],
-		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, rem, earum ratione blanditiis itaque esse laudantium consequuntur repellat nisi possimus eius velit quos? Iusto, ex dignissimos et quas laboriosam soluta voluptatem quaerat at iste repudiandae atque sed perspiciatis nihil recusandae in deserunt inventore quibusdam. Praesentium, fugit, molestias, dolor modi esse quo itaque excepturi culpa sed voluptatem facilis blanditiis ratione mollitia ex minima unde consequatur autem harum qui placeat odio repellendus laudantium. Alias, ducimus aut provident. Quod, mollitia, facere enim amet minus ipsam dolorem magnam et aspernatur tenetur ut aperiam natus beatae incidunt debitis architecto est animi qui aliquid assumenda esse nulla ipsa voluptatum deleniti iure reiciendis in repudiandae dolorum praesentium tempore ex quaerat aut quae quibusdam corrupti perspiciatis expedita neque. Iure, expedita, dignissimos consequatur laboriosam facilis quidem quas assumenda atque vero ducimus numquam perspiciatis dolorem dolor labore molestias corporis ullam. Cupiditate, pariatur, tenetur harum consequuntur earum qui minus officiis debitis possimus ducimus voluptate excepturi explicabo eius cumque nesciunt libero fugiat delectus doloribus aliquid quod veniam sit nostrum deserunt tempora deleniti autem rem quae hic perspiciatis! Voluptas, odio, quam, debitis assumenda amet excepturi laborum distinctio harum eos quidem illo sapiente. Unde, earum, numquam inventore nulla doloribus labore aliquid ipsum possimus qui."
-	});
-
-	var postThree = new Post({
-		title: "test",
-		tags: ['Hello', 'world', 'how are you'],
-		content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, rem, earum ratione blanditiis itaque esse laudantium consequuntur repellat nisi possimus eius velit quos? Iusto, ex dignissimos et quas laboriosam soluta voluptatem quaerat at iste repudiandae atque sed perspiciatis nihil recusandae in deserunt inventore quibusdam. Praesentium, fugit, molestias, dolor modi esse quo itaque excepturi culpa sed voluptatem facilis blanditiis ratione mollitia ex minima unde consequatur autem harum qui placeat odio repellendus laudantium. Alias, ducimus aut provident. Quod, mollitia, facere enim amet minus ipsam dolorem magnam et aspernatur tenetur ut aperiam natus beatae incidunt debitis architecto est animi qui aliquid assumenda esse nulla ipsa voluptatum deleniti iure reiciendis in repudiandae dolorum praesentium tempore ex quaerat aut quae quibusdam corrupti perspiciatis expedita neque. Iure, expedita, dignissimos consequatur laboriosam facilis quidem quas assumenda atque vero ducimus numquam perspiciatis dolorem dolor labore molestias corporis ullam. Cupiditate, pariatur, tenetur harum consequuntur earum qui minus officiis debitis possimus ducimus voluptate excepturi explicabo eius cumque nesciunt libero fugiat delectus doloribus aliquid quod veniam sit nostrum deserunt tempora deleniti autem rem quae hic perspiciatis! Voluptas, odio, quam, debitis assumenda amet excepturi laborum distinctio harum eos quidem illo sapiente. Unde, earum, numquam inventore nulla doloribus labore aliquid ipsum possimus qui."
-	});
-
-	postOne.save(savePostCallback);
-	postTwo.save(savePostCallback);
-	postThree.save(savePostCallback);
-
-	function savePostCallback(err, post) {
-		if (err) return console.error(err);
-		console.log(post.title + ' saved to database.');
-	}
 
 	app.get('/', function(req, res) {
 		res.render('index');
 	});
 
+	// Returns a list of post objects
 	app.get('/db/posts', function(req, res) {
 		Post.find(function(err, docs) {
 			if (err) return console.error(err);
@@ -90,13 +62,14 @@ db.once('open', function() {
 		});
 	});
 
-	// database crud methods
-	app.get('/db/post/:id', function(req, res) {
-		Post.find({ title: /^test/ }, function(err, docs) {
-			if (err) return console.error(err);
-			res.send(docs);
+	// Create a single post object
+	app.post('/db/posts', function(req, res) {
+		var post = new Post({
+			title: req.body.title,
+
 		});
 	});
+
 
 	app.listen(app.get('PORT'));
 
--- todo.md
+++ todo.md
@@ -4,15 +4,15 @@ A mean blog built with mean tools
 
 ##Application overview
 
-As a goal, this application will provide basic blogging functionality using the MEAN stack. This includes front end theming capabilities, as well as back end editing and administration abilities. 
+As a goal, this application will provide basic blogging functionality using the MEAN stack. This includes front end theming capabilities, as well as back end editing and administration abilities.
 
 ##Development goals
- 
- - Learn more about the MEAN stack 
+
+ - Learn more about the MEAN stack
  - Use TDD through the entire client and server side development
 
-##Feature list 
-	
+##Feature list
+
  - Front end
 	 - Front end chronological display of blog posts
 	 - Allow for custom theming of the front end
@@ -20,24 +20,27 @@ As a goal, this application will provide basic blogging functionality using the 
 	 - Search functionality
  - Back end
  	- Blog posts saved in local mongodb database
- 	- Editing in markdown 
+ 	- Editing in markdown
  	- Session/user support
  	- User roles/variable permissions
 
 ##User stories
 
-These are discrete goals that an end-user would recognize as a new feature or update. 
+These are discrete goals that an end-user would recognize as a new feature or update.
  * Using pre-populated sample data, render a blog home page with x number of posts
- 	* Set up back end REST API to deliver mongodb data to the front end 
+ 	* Set up back end REST API to deliver mongodb data to the front end
  	- Get angular to read the data
  	- Use angular to populate a front page
  - Set up a user and permissions system to log into a backend
  - Allow accessing a backend to create new posts
  - Allow backend access to see older posts and edit them
  - Allow backend access to delete old posts
- 
+
 ##Programming tasks
 
+ - Update to express 4.x and set up the new middleware
  - Set up and test a connection to the local 'meanblog' database
- 
+ - Might have to move away from express3-handlebars
+
+
 ##Blog post data structure
