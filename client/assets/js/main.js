1163c9074afee65702a779d8258bcfce6b3a8599
Chris Honiball <chris.p.honiball@gmail.com>
2014-04-23 12:13:18 -0500

    Angular main view now retrieves listing of post, allows creation of new posts and deleition of existing posts. Need to figure out testing now.

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,5 +1,39 @@
 var app = angular.module('meanblog', ['ngResource']); 
 
-app.controller('PostCtrl', function($scope) {
-	$scope.posts = 'Here is a post listing sample'; 
+app.config(['$resourceProvider', function($resourceProvider) { // configure resource factory/provider here
+
+}]); 
+
+app.controller('PostCtrl', function($scope ,$resource) {
+	var Posts = $resource('http://localhost:1337/posts/:id', {}, {
+		update: {
+			method: 'PUT'
+		}
+	}); 
+
+	$scope.showPosts = false;
+	$scope.posts; 
+
+	$scope.loadPosts = function() {
+		$scope.posts = 	Posts.query(function() {
+			$scope.showPosts = true; 
+		});
+	};
+
+	$scope.deletePost = function(id) {
+		var deletedPost = Posts.delete({id: id}, function() {
+			$scope.posts = Posts.query();
+		});
+	};
+
+	$scope.createPost = function(post) {
+		Posts.save({
+			title: post.title, 
+			content: post.content, 
+			tags: post.tags
+		}, function() {
+			$scope.posts = Posts.query(); 
+		}); 
+	};
+	
 }); 
